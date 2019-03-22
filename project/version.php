69ba3ef6086a19bbf2e0bf8aad9c16807c6708aa
Robert Johnk <bobprince.raj@gmail.com>
2019-03-22 09:20:03 -0700

    Update version.php

--- project/version.php
+++ project/version.php
@@ -1,6 +1,6 @@
 <?php
-$plugin->component = 'block_Quotes';
-$plugin->version = 2019032200;
-$plugin->requires = 2018120302.11;
-$plugin->maturity = MATURITY_ALPHA;
-$plugin->release = 'v0.0';
+$plugin->component = 'block_Quotes'; //plugin is a block named Quotes
+$plugin->version = 2019032200; //Date YYYYMMDD## (update every time)
+$plugin->requires = 2018120302.11; //Moodle version number
+$plugin->maturity = MATURITY_ALPHA; //Development phase
+$plugin->release = 'v0.0'; //plugin version number (update every time)
